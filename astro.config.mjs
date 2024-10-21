34821444eedba346e7d8410a4236262ea91a4c35
aretwojay <rubenmuya9129@gmail.com>
2024-10-21 12:29:28 +0200

    setup Astro.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Must be 'static' or 'hybrid'
+  output: "static",
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
